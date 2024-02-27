a48d7b765176b107246ac59dec861be2aeb26c70
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2024-02-27 21:48:37 +0200

    wip: parsing expressions

--- src/parse/parser.py
+++ src/parse/parser.py
@@ -98,8 +98,46 @@ class Parser:
 
         self.nl()
 
+    def term(self):
+        """
+        term ::= unary {( "*" | "/" ) unary}
+        """
+        logging.info("term")
+        self.unary()
+        while self.check_token(TokenType.ASTERISK) or self.check_token(TokenType.SLASH):
+            self.next_token()
+            self.unary()
+
+    def unary(self):
+        """
+        unary ::= ["+" | "-"] primary
+        """
+        logging.info("unary")
+        if self.check_token(TokenType.PLUS) or self.check_token(TokenType.MINUS):
+            self.next_token()
+        self.primary()
+
+    def primary(self):
+        """
+        primary ::= number | ident
+        """
+        logging.info("primary")
+        if self.check_token(TokenType.NUMBER):
+            self.next_token()
+        elif self.check_token(TokenType.IDENT):
+            self.next_token()
+        else:
+            self.abort(f"Unexpected token at {self.current_token.value}")
+
     def expression(self):
-        ...
+        """
+        expression ::= term {( "-" | "+" ) term}
+        """
+        logging.info("expression")
+        self.term()
+        while self.check_token(TokenType.PLUS) or self.check_token(TokenType.MINUS):
+            self.next_token()
+            self.term()
 
     def comparison(self):
         """
@@ -114,6 +152,10 @@ class Parser:
             self.expression()
         else:
             self.abort(f"Expected comparison operator at {self.current_token.value}")
+
+        while self.is_comparison_operator():
+            self.next_token()
+            self.expression()
 
     def is_comparison_operator(self) -> bool:
         return self.check_token(TokenType.EQEQ) or self.check_token(TokenType.NOTEQ) or self.check_token(
