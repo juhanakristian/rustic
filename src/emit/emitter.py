26885dae6d26d611b932aa7a25287b684536488f
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2024-07-29 11:35:56 +0300

    wip: emitter

--- src/emit/emitter.py
+++ src/emit/emitter.py
@@ -2,11 +2,17 @@ from src.lex.lexer import TokenType
 from src.ast.nodes import (
     ASTNode,
     BinaryOpNode,
+    ComparisonNode,
+    IfNode,
     LetNode,
     PrimaryNode,
     PrintNode,
     ProgramNode,
 )
+
+
+class EmitError(Exception):
+    pass
 
 
 class Emitter:
@@ -23,15 +29,15 @@ class Emitter:
         for statement in self.ast.statements:
             output += self.emit_node(statement)
 
-        program_code = "fn main() {\n" + output + "\n}"
+        program_code = "fn main() {\n" + output + "}"
 
         return program_code
 
     def emit_node(self, node: ASTNode) -> str:
         if isinstance(node, PrintNode):
-            return f'println("{node.value}")'
+            return f'println("{node.value}");\n'
         elif isinstance(node, LetNode):
-            return f"let {node.variable} = {self.emit_node(node.expression)}"
+            return f"let {node.variable} = {self.emit_node(node.expression)};\n"
         elif isinstance(node, PrimaryNode):
             return f"{node.value}"
         elif isinstance(node, BinaryOpNode):
@@ -44,5 +50,37 @@ class Emitter:
             return (
                 f"{self.emit_node(node.left)} {operator} {self.emit_node(node.right)}"
             )
+        elif isinstance(node, IfNode):
+            comparison = self.emit_node(node.condition)
+
+            then_block = ""
+            for block_node in node.then_branch:
+                then_block += self.emit_node(block_node)
+
+            return f"if {comparison} {{\n{then_block}}}\n"
+        elif isinstance(node, ComparisonNode):
+            left_node = self.emit_node(node.left)
+            right_node = self.emit_node(node.right)
+
+            comparison = None
+            if node.operator == TokenType.EQ:
+                comparison = "=="
+            elif node.operator == TokenType.EQEQ:
+                comparison = "=="
+            elif node.operator == TokenType.NOTEQ:
+                comparison = "!="
+            elif node.operator == TokenType.LT:
+                comparison = "<"
+            elif node.operator == TokenType.LTEQ:
+                comparison = "<="
+            elif node.operator == TokenType.GT:
+                comparison = ">"
+            elif node.operator == TokenType.GTEQ:
+                comparison = ">="
+
+            if comparison is None:
+                raise EmitError(f"Invalid comparison operator {node.operator}")
+
+            return f"{left_node} {comparison} {right_node}"
 
         return ""
--- tests/test_emitter.py
+++ tests/test_emitter.py
@@ -13,7 +13,7 @@ def test_print_emit():
 
     output = emitter.emit()
 
-    assert output == 'fn main() {\nprintln("hello")\n}'
+    assert output == 'fn main() {\nprintln("hello");\n}'
 
 
 def test_basic_expressions_emit():
@@ -22,7 +22,19 @@ def test_basic_expressions_emit():
     ast = parser.program()
 
     emitter = Emitter(ast)
-
     output = emitter.emit()
 
-    assert output == "fn main() {\nlet foo = 3 + 2\n}"
+    assert output == "fn main() {\nlet foo = 3 + 2;\n}"
+
+
+def test_basic_conditional_emit():
+    lexer = Lexer('let foo = 3 + 2\nif foo > 0 then\nprint "yes"\nendif\n')
+    parser = Parser(lexer)
+    ast = parser.program()
+
+    emitter = Emitter(ast)
+    output = emitter.emit()
+
+    assert (
+        output == 'fn main() {\nlet foo = 3 + 2;\nif foo > 0 {\nprintln("yes");\n}\n}'
+    )
